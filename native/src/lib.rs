e4e39bc9e57ff4f0390f17c4f2d09c671d79a124
Matt Bell <mappum@gmail.com>
2019-08-29 17:57:34 -0700

    Implementing get method

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,13 +3,40 @@ extern crate neon;
 extern crate merk;
 
 use std::path::Path;
+use merk::Merk;
 use neon::prelude::*;
 
-fn open(mut cx: FunctionContext) -> JsResult<JsString> {
-    let path = cx.argument::<JsString>(0)?;
-    Ok(cx.string("hello node"))
+pub struct MerkHandle {
+    store: Merk
 }
 
-register_module!(mut cx, {
-    cx.export_function("open", open)
-});
+// TODO: throw instead of panicking
+
+declare_types! {
+  pub class JsMerk for MerkHandle {
+    init(mut cx) {
+        let path = cx.argument::<JsString>(0)?.value();
+        let path = Path::new(&path);
+        let store = Merk::open(path).unwrap();
+        Ok(MerkHandle { store })
+    }
+
+    method get(mut cx) {
+        let key = cx.argument::<JsBuffer>(0)?;
+        let this = cx.this();
+        let value = cx.borrow(&key, |key| {
+            let guard = cx.lock();
+            let handle = this.borrow(&guard);
+            handle.store.get(key.as_slice()).unwrap()
+        });
+
+        let buffer = cx.buffer(value.len() as u32)?;
+        for i in 0..value.len() {
+            let n = cx.number(value[i]);
+            buffer.set(&mut cx, i as u32, n)?;
+        }
+        Ok(buffer.upcast())
+    }
+  }
+}
+register_module!(mut m, { m.export_class::<JsMerk>("Merk") });
