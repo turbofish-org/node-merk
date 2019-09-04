134243d7c25bfd74daf97960c6a6047d5d823a2e
Matt Bell <mappum@gmail.com>
2019-09-04 16:24:40 -0700

    Flush Merk when process exits

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,19 +4,22 @@ extern crate merk;
 
 use std::collections::BTreeMap;
 use std::path::Path;
+use std::rc::Rc;
+use std::sync::Mutex;
 use merk::{Merk, Op};
 use neon::prelude::*;
 
 pub struct MerkHandle {
-    store: Merk
+    store: Rc<Mutex<Merk>>
 }
 
 pub struct Batch {
-    ops: BTreeMap<Vec<u8>, Op>
+    ops: Option<BTreeMap<Vec<u8>, Op>>,
+    store: Rc<Mutex<Merk>>
 }
 
 // TODO: throw instead of panicking
-
+// TODO: make this code succinct
 
 macro_rules! buffer_arg_to_vec {
     ($cx:ident, $index:expr) => {
@@ -36,17 +39,18 @@ declare_types! {
             let path = cx.argument::<JsString>(0)?.value();
             let path = Path::new(&path);
             let store = Merk::open(path).unwrap();
-            Ok(MerkHandle { store })
+            Ok(MerkHandle { store: Rc::new(Mutex::new(store)) })
         }
 
-        method get_sync(mut cx) {
+        method getSync(mut cx) {
             let key = buffer_arg_to_vec!(cx, 0);
 
             let value = {
                 let this = cx.this();
                 let guard = cx.lock();
                 let handle = this.borrow(&guard);
-                handle.store.get(key.as_slice()).unwrap()
+                let store = handle.store.lock().unwrap();
+                store.get(key.as_slice()).unwrap()
             };
 
             let buffer = cx.buffer(value.len() as u32)?;
@@ -57,49 +61,122 @@ declare_types! {
             Ok(buffer.upcast())
         }
 
+        method rootHash(mut cx) {
+            let hash = {
+                let this = cx.this();
+                let guard = cx.lock();
+                let handle = this.borrow(&guard);
+                let store = handle.store.lock().unwrap();
+                store.root_hash()
+            };
+
+            let buffer = cx.buffer(20)?;
+            for i in 0..20 {
+                let n = cx.number(hash[i]);
+                buffer.set(&mut cx, i as u32, n)?;
+            }
+            Ok(buffer.upcast())
+        }
+
         method batch(mut cx) {
-            let args: Vec<Handle<JsValue>> = vec![];
+            let args: Vec<Handle<JsMerk>> = vec![ cx.this() ];
             Ok(JsBatch::new(&mut cx, args)?.upcast())
         }
-    }
 
-    pub class JsBatch for Batch {
-        init(_cx) {
-            Ok(Batch { ops: BTreeMap::new() })
-        }
-
-        method put(mut cx) {
+        method flushSync(mut cx) {
             {
-                let key = buffer_arg_to_vec!(cx, 0);
-                let value = buffer_arg_to_vec!(cx, 1);
-                // TODO: assert lengths
-
-
-                let mut this = cx.this();
+                let this = cx.this();
                 let guard = cx.lock();
-                let mut handle = this.borrow_mut(&guard);
-                handle.ops.insert(key, Op::Put(value));
+                let handle = this.borrow(&guard);
+                let store = handle.store.lock().unwrap();
+                store.flush().unwrap();
             }
 
             Ok(cx.undefined().upcast())
         }
+    }
+
+    pub class JsBatch for Batch {
+        init(mut cx) {
+            let merk = cx.argument::<JsMerk>(0)?;
+            let guard = cx.lock();
+            let handle = merk.borrow(&guard);
+            Ok(Batch {
+                ops: Some(BTreeMap::new()),
+                store: handle.store.clone()
+            })
+        }
+
+        method put(mut cx) {
+            let res = {
+                let key = buffer_arg_to_vec!(cx, 0);
+                let value = buffer_arg_to_vec!(cx, 1);
+                // TODO: assert lengths
+
+                let mut this = cx.this();
+                let guard = cx.lock();
+                let mut handle = this.borrow_mut(&guard);
+
+                if let Some(ops) = &mut handle.ops {
+                    ops.insert(key, Op::Put(value));
+                    Ok(())
+                } else {
+                    Err("batch was already committed")
+                }
+            };
+
+            match res {
+                Ok(_) => Ok(cx.this().upcast()),
+                Err(err) => cx.throw_error(err)
+            }
+        }
 
         method delete(mut cx) {
-            {
+            let res = {
                 let key = buffer_arg_to_vec!(cx, 0);
                 // TODO: assert length
 
                 let mut this = cx.this();
                 let guard = cx.lock();
                 let mut handle = this.borrow_mut(&guard);
-                handle.ops.insert(key, Op::Delete);
-            }
 
-            Ok(cx.undefined().upcast())
+                if let Some(ops) = &mut handle.ops {
+                    ops.insert(key, Op::Delete);
+                    Ok(())
+                } else {
+                    Err("batch was already committed")
+                }
+            };
+
+            match res {
+                Ok(_) => Ok(cx.this().upcast()),
+                Err(err) => cx.throw_error(err)
+            }
         }
 
-        method commit_sync(mut cx) {
-            cx.throw_error("not yet implemented")
+        method commitSync(mut cx) {
+            let mut do_commit = || {
+                let mut this = cx.this();
+                let guard = cx.lock();
+                let mut handle = this.borrow_mut(&guard);
+
+                if let Some(ops) = handle.ops.take() {
+                    let mut batch = Vec::with_capacity(ops.len());
+                    for entry in ops {
+                        batch.push(entry);
+                    }
+                    let mut store = handle.store.lock().unwrap();
+                    store.apply(batch.as_slice()).unwrap();
+                    Ok(())
+                } else {
+                    Err("batch was already committed")
+                }
+            };
+
+            match do_commit() {
+                Ok(_) => Ok(cx.undefined().upcast()),
+                Err(err) => cx.throw_error(err)
+            }
         }
 
         method commit(mut cx) {
